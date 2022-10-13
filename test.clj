f5bdba8f9fe0da7adad733d82694966209b69d2c
Bobby Towers <btowers793@gmail.com>
2022-10-12 19:29:11 -0700

    Update test.clj

--- test.clj
+++ test.clj
@@ -2,6 +2,9 @@
 
 (require '[babashka.fs :as fs])
 
-(fs/unzip (fs/file (fs/cwd) "./main/clj-kondo"))
+(fs/unzip 
+  (fs/file 
+    (fs/path (fs/cwd) "/main/") 
+    ".clj-kondo"))
 
 (prn (fs/list-dir (fs/path (fs/cwd) "./main/resources/twofers/")))
