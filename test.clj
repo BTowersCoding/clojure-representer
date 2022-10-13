880d46f6b9c71822b84bda15adcc00e409833e59
Bobby Towers <btowers793@gmail.com>
2022-10-12 17:53:30 -0700

    Update test.clj

--- test.clj
+++ test.clj
@@ -4,52 +4,10 @@
  '[cheshire.core :as json]
  '[clojure.string :as str]
  '[clojure.java.shell :as shell]
- '[clojure.java.io :as io])
+ '[clojure.java.io :as io]
+ '[babashka.fs :as fs])
 
-(def root (str (fs/parent *file*) "/"))
+(fs/unzip "clj-kondo")
 
-(def test-runner-dir (str (fs/parent *file*) "/"))
-
-(defn- ->snake_case [s] (str/replace s \- \_))
-
-(def practice-exercises
-  (map #(% "slug")
-       (-> (str root "config.json")
-           slurp
-           json/parse-string
-           (get "exercises")
-           (get "practice"))))
-
-(def concept-exercises
-  (map #(% "slug")
-       (-> (str root "config.json")
-           slurp
-           json/parse-string
-           (get "exercises")
-           (get "concept"))))
-
-(defn test-exercise [slug]
-  (let [practice? (contains? (set practice-exercises) slug)
-        example (if practice?
-                  (str root "exercises/practice/" slug "/.meta/src/example.clj")
-                  (str root "exercises/concept/" slug "/.meta/exemplar.clj"))
-        src (if practice?
-              (str root "exercises/practice/" slug "/src/" (->snake_case slug) ".clj")
-              (str root "exercises/concept/" slug "/src/" (->snake_case slug) ".clj"))]
-    (shell/sh "cp" example src)
-    (= "pass" ((json/parse-string
-                (:out (shell/sh (str test-runner-dir "test-runner.clj")
-                                slug
-                                (str root (if practice? "exercises/practice/" "exercises/concept/") slug "/")
-                                (str root (if practice? "exercises/practice/" "exercises/concept/") slug "/"))))
-               "status"))))
-
-(defn test-exercises! []
-  (for [exercise (into practice-exercises concept-exercises)]
-    {(keyword exercise) (test-exercise exercise)}))
-
-(let [results (test-exercises!)
-      fails (filter #(false? (first (vals %))) results)]
-  (prn {:tested (count results)
-        :fails fails})
-  (System/exit (count fails)))
+(prn
+  (fs/list-dir "resources/twofers/"))
