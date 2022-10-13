db2701b8328c2f07ebb163e306f9b04aaae7809d
Bobby Towers <btowers793@gmail.com>
2022-10-12 19:22:25 -0700

    Update test.clj

--- test.clj
+++ test.clj
@@ -1,14 +1,7 @@
 #!/usr/bin/env bb
 
-(require
- '[cheshire.core :as json]
- '[clojure.string :as str]
- '[clojure.java.shell :as shell]
- '[clojure.java.io :as io]
- '[babashka.fs :as fs])
+(require '[babashka.fs :as fs])
 
-(prn (fs/cwd))
+(fs/unzip (fs/file (fs/cwd) "/main/clj-kondo"))
 
-(fs/unzip "./main/clj-kondo")
-
-(prn (fs/list-dir "./main/resources/twofers/"))
+(prn (fs/list-dir (fs/path (fs/cwd) "/main/resources/twofers/")))
