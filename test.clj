dd0307fb597398a47167a084148822a841ed946d
Bobby Towers <btowers793@gmail.com>
2022-10-12 21:02:45 -0700

    Update test.clj

--- test.clj
+++ test.clj
@@ -13,7 +13,7 @@
   (path-str "." "main") 
     "clj-kondo-2022.10.05-linux-static-amd64.zip"))
 
-(fs/glob (path-str "." "main") "*clj-kondo*"))
+(fs/glob (path-str "." "main") "*clj-kondo*")
 
 (let [paths (fs/list-dir (path-str "." "main" "resources" "twofers"))
       solutions (map #(slurp (fs/file % "two_fer.clj")) paths)
