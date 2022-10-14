de39273267396d08036ffbf144348801fdbc43ed
Bobby Towers <btowers793@gmail.com>
2022-10-13 19:55:42 -0700

    Update test.clj

--- test.clj
+++ test.clj
@@ -9,22 +9,20 @@
       str))
 
 (fs/unzip 
-  (fs/file
-    (path-str "." "main") 
-    "clj-kondo-2022.10.05-linux-static-amd64.zip")
+  (fs/file "." "clj-kondo-2022.10.05-linux-static-amd64.zip")
     "/home/runner/.local/bin")
 
 (fs/set-posix-file-permissions "/home/runner/.local/bin/clj-kondo" "rwxrwxrwx")
 
-(let [paths (fs/list-dir (path-str "." "main" "resources" "twofers"))
+(let [paths (fs/list-dir (path-str "." "resources" "twofers"))
       solutions (map #(slurp (fs/file % "two_fer.clj")) paths)
       unique (set solutions)]
     (println (str (count solutions) " total solutions"))
     (println (str (count unique) " unique solutions"))
     (doseq [path (take 5 paths)]
-      (let [representation (:out (sh/sh "bb" "main/clojure_representer.clj"
+      (let [representation (:out (sh/sh "bb" "clojure_representer.clj"
                            "two-fer" (str path) (str path)))]
         (println "Source")
         (prn (slurp (fs/file path "two_fer.clj")))
         (println "Representation")
-        (prn representation))))
+        (prn (slurp (fs/file path "two_fer.clj"))))))
